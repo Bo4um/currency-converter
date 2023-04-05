18a74f8f3f539986e46de87099ec458a12475c72
Dmitry Belyatskiy <dmitry.belyatskiy@gmail.com>
2023-04-05 20:16:02 +0300

    added patterns for toDollars and toRubles methods

--- src/main/java/modsen/practics/App.java
+++ src/main/java/modsen/practics/App.java
@@ -1,13 +1,27 @@
 package modsen.practics;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App
 {
+
+    static String dollarsPattern = "toDollars\\((\\d+,\\d+\\)|.)";
+    static String rublesPattern = "toRubles\\((\\d+,\\d+\\)|.)";
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Pattern pattern = Pattern.compile(dollarsPattern);
+        Matcher matcher = pattern.matcher(str);
+        while(matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            System.out.println("Found: " + str.subSequence(start, end)
+                    + " starts at " + start
+                    + " and ends at " + (end - 1));
+        }
     }
 }
